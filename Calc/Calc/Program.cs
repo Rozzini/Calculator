9b3624fd3120569c9a9043f8e454e87bd4eee25e
Rozzini <Rozzinimr@gmail.com>
2020-12-22 00:39:48 +0300

    Created calss calc

--- Calc/Calc/Program.cs
+++ Calc/Calc/Program.cs
@@ -4,9 +4,23 @@ namespace Calc
 {
     class Program
     {
+
+        class Calc
+        {
+            public int plus(int x, int y)
+            {
+                return x + y;
+            }
+            public int minus(int x, int y)
+            {
+                return x - y;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calc calc = new Calc();
+
+            Console.WriteLine(calc.plus(5, 2));
         }
     }
 }
